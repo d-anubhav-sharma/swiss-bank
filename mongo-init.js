3cc00a9fd9d7c1d0f01f52a33f5eb56a209dc629
anubhav8804602148 <anubhav8804602148@gmail.com>
2024-06-17 01:23:49 +0530

    User role seeding

--- mongo-init.js
+++ mongo-init.js
@@ -1,4 +1,4 @@
-db = db.getSiblingDB('admin');
+db = db.getSiblingDB("admin");
 
 db.createUser({
   user: "swissbank",
@@ -7,6 +7,30 @@ db.createUser({
     { role: "root", db: "admin" },
     { role: "userAdminAnyDatabase", db: "admin" },
     { role: "dbAdminAnyDatabase", db: "admin" },
-    { role: "readWriteAnyDatabase", db: "admin" }
-  ]
+    { role: "readWriteAnyDatabase", db: "admin" },
+  ],
+});
+
+db.collection("user").insertOne({
+  _id: {
+    $oid: "666080e310d3317ab0c0c8c6",
+  },
+  username: "anubhav_sharma",
+  password: "$2a$10$r9LwHulRCDMMU2fHpyY29OKqFuuLjWmGPLE.6HvdzTLthF0sjS652",
+  email: "anubhav.sharma@swiss-bank.com",
+  roles: [
+    {
+      $oid: "6660810710d3317ab0c0c8c7",
+    },
+  ],
+  _class: "com.swiss.bank.user.service.entities.User",
+});
+db.collection("role").insertOne({
+  _id: {
+    $oid: "6660810710d3317ab0c0c8c7",
+  },
+  username: "anubhav_sharma",
+  roleName: "OWNER",
+  privileges: ["STAFF", "ADMIN"],
+  _class: "com.swiss.bank.user.service.entities.Role",
 });
